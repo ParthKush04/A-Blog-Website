d4b1059532e1b62f6bd007a9017cd6dbdf05e2de
pri2003yam <132195934+pri2003yam@users.noreply.github.com>
2024-04-12 21:57:50 +0530

    login

---
+++ NODE/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Login from './components/accounts/login';
+
+function App() {
+  return (
+    <div style={{marginTop: 64}}>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
