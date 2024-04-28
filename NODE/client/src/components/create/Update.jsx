807a77033ee1625494e57dac5c859e90dd5f3c6a
Parth Kushwaha <parthkush1000@gmail.com>
2024-04-28 12:31:15 +0530

    update

--- NODE/client/src/components/create/Update.jsx
+++ NODE/client/src/components/create/Update.jsx
@@ -56,6 +56,7 @@ const Updates = () =>{
   const {account} = useContext(DataContext);
 
   const location = useLocation();
+  const { id } = useParams();
   const navigate = useNavigate();
   const url = post.picture? post.picture : 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
 
